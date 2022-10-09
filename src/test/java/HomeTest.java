32b4613a5997ed9936fe186bcf17565299fb653a
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2022-10-09 09:25:40 -0400

    US34 completed

--- src/test/java/HomeTest.java
+++ src/test/java/HomeTest.java
@@ -1,6 +1,15 @@
+import org.testng.annotations.Test;
+
 public class HomeTest {
     String name = "Kuba";
     String role = "Instructor";
     int id = 99;
     String email = "kuba@test.com";
+
+    @Test(testName = "Testing title of the page")
+    public void test01(){
+        //implementation
+    }
+
+
 }
