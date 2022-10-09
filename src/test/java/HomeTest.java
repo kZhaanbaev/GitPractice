4478ff9da4ef8678d1c3bf1c5738c0a10a006745
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2022-10-09 09:29:01 -0400

    US43 completed

--- src/test/java/HomeTest.java
+++ src/test/java/HomeTest.java
@@ -6,10 +6,16 @@ public class HomeTest {
     int id = 99;
     String email = "kuba@test.com";
 
-    @Test(testName = "Testing title of the page")
+    @Test(testName = "US34: Testing title of the page")
     public void test01(){
         //implementation
     }
 
+    @Test(testName = "US43: Testing employee input table")
+    public void test02(){
+        //implementation part 1
+        //use switchToNewWindow method
+        //completed test case
+    }
 
 }
